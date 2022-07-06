93b5bf8a8d8e0569e8983f1204abfc2749ace0ed
Karan Ahlawat <ahlawatkaran12@gmail.com>
2022-07-06 12:48:04 +0530

    [add] I think this works well tbh

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import vue from "@vitejs/plugin-vue"
 
-export default defineConfig(({ command }) => {
-  return {
+export default defineConfig({
     root: './client',
     build: {
-      manifest: true,
-      outDir: '../resources/',
-      emptyOutDir: true
+        manifest: true,
+        outDir: '../resources/',
+        emptyOutDir: true
     },
     plugins: [
-      vue()
+        vue()
     ]
-  }
 });
